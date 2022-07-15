96f68444960eaa24a6a7890837e983577b6abfba
Jure Demsar <demsarjure@gmail.com>
2022-07-15 11:24:37 -0400

    Added an additional plot.

---  normal_plot.R
+++  normal_plot.R
@@ -21,6 +21,26 @@ ggplot(df_plot, aes(x = diff, y = mu)) +
                      breaks = c(0, 1, 2),
                      labels = c("Pre", "During", "Post"))
 
+ggsave("./fig/merged_no.tiff",
+       width = 1920,
+       height = 1920,
+       dpi = 250,
+       units = "px")
+
+# remove no
+df_plot <- df_plot %>%
+  filter(condition != "No")
+
+# plot
+ggplot(df_plot, aes(x = diff, y = mu)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
+  stat_pointinterval() +
+  ylab("Difference") +
+  facet_grid(type ~ condition, scales = "free_y", labeller = label_parsed) +
+  scale_x_continuous(name = "",
+                     breaks = c(0, 1, 2),
+                     labels = c("Pre", "During", "Post"))
+
 ggsave("./fig/merged.tiff",
        width = 1920,
        height = 1920,
