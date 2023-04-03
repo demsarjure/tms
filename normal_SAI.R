0b31d9c70d3ea80d696cc21263736ff271fa3e05
Jure Demsar <demsarjure@gmail.com>
2023-04-03 14:12:17 +0200

    Some additional visualizations.

--- normal_SAI.R
+++ normal_SAI.R
@@ -18,116 +18,243 @@ model <- cmdstan_model("./models/cauchy.stan")
 # ISIs -------------------------------------------------------------------------
 # real
 r <- fit_and_compare(df_real_stim$ISI20_diff, "(Real stim - real pre)")
+df_samples_isi20 <- data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "taVNS"
+)
 
 r <- fit_and_compare(df_real_stim$ISI22_diff, "(Real stim - real pre)")
-df_samples <- data.frame(mu = r$mu,
-                         diff = 1,
-                         condition = "taVNS")
+df_samples_isi22 <- data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "taVNS"
+)
 
 r <- fit_and_compare(df_real_stim$ISI24_diff, "(Real stim - real pre)")
+df_samples_isi24 <- data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "taVNS"
+)
+
 r <- fit_and_compare(df_real_post$ISI20_diff, "(Real post - real pre)")
+df_samples_isi20 <- df_samples_isi20 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "taVNS"
+))
 
 r <- fit_and_compare(df_real_post$ISI22_diff, "(Real post - real pre)")
-df_samples <- df_samples %>% add_row(data.frame(mu = r$mu,
-                                     diff = 2,
-                                     condition = "taVNS"))
+df_samples_isi22 <- df_samples_isi22 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "taVNS"
+))
 
 r <- fit_and_compare(df_real_post$ISI24_diff, "(Real post - real pre)")
+df_samples_isi24 <- df_samples_isi24 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "taVNS"
+))
 
 # sham
 r <- fit_and_compare(df_sham_stim$ISI20_diff, "(Sham stim - sham pre)")
+df_samples_isi20 <- df_samples_isi20 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "sVNS"
+))
 
 r <- fit_and_compare(df_sham_stim$ISI22_diff, "(Sham stim - sham pre)")
-df_samples <- df_samples %>% add_row(data.frame(mu = r$mu,
-                                     diff = 1,
-                                     condition = "sVNS"))
+df_samples_isi22 <- df_samples_isi22 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "sVNS"
+))
 
 r <- fit_and_compare(df_sham_stim$ISI24_diff, "(Sham stim - sham pre)")
+df_samples_isi24 <- df_samples_isi24 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "sVNS"
+))
+
 r <- fit_and_compare(df_sham_post$ISI20_diff, "(Sham post - sham pre)")
+df_samples_isi20 <- df_samples_isi20 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "sVNS"
+))
 
 r <- fit_and_compare(df_sham_post$ISI22_diff, "(Sham post - sham pre)")
-df_samples <- df_samples %>% add_row(data.frame(mu = r$mu,
-                                     diff = 2,
-                                     condition = "sVNS"))
+df_samples_isi22 <- df_samples_isi22 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "sVNS"
+))
 
 r <- fit_and_compare(df_sham_post$ISI24_diff, "(Sham post - sham pre)")
+df_samples_isi24 <- df_samples_isi24 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "sVNS"
+))
 
 # no
 r <- fit_and_compare(df_no_stim$ISI20_diff, "(No stim - no pre)")
+df_samples_isi20 <- df_samples_isi20 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "xVNS"
+))
+
 
 r <- fit_and_compare(df_no_stim$ISI22_diff, "(No stim - no pre)")
-df_samples <- df_samples %>% add_row(data.frame(mu = r$mu,
-                                     diff = 1,
-                                     condition = "xVNS"))
+df_samples_isi22 <- df_samples_isi22 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "xVNS"
+))
 
 r <- fit_and_compare(df_no_stim$ISI24_diff, "(No stim - no pre)")
+df_samples_isi24 <- df_samples_isi24 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 1,
+  condition = "xVNS"
+))
+
 r <- fit_and_compare(df_no_post$ISI20_diff, "(No post - no pre)")
+df_samples_isi20 <- df_samples_isi20 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "xVNS"
+))
 
 r <- fit_and_compare(df_no_post$ISI22_diff, "(No post - no pre)")
-df_samples <- df_samples %>% add_row(data.frame(mu = r$mu,
-                                     diff = 2,
-                                     condition = "xVNS"))
+df_samples_isi22 <- df_samples_isi22 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "xVNS"
+))
 
 r <- fit_and_compare(df_no_post$ISI24_diff, "(No post - no pre)")
+df_samples_isi24 <- df_samples_isi24 %>% add_row(data.frame(
+  mu = r$mu,
+  diff = 2,
+  condition = "xVNS"
+))
 
 # ISI22 ------------------------------------------------------------------------
 # add dummy entries
-df_samples <- df_samples %>%
+df_samples_isi20 <- df_samples_isi20 %>%
   add_row(data.frame(mu = 0, diff = 0, condition = "taVNS"))
-df_samples <- df_samples %>%
+df_samples_isi20 <- df_samples_isi20 %>%
   add_row(data.frame(mu = 0, diff = 0, condition = "sVNS"))
-df_samples <- df_samples %>%
+df_samples_isi20 <- df_samples_isi20 %>%
   add_row(data.frame(mu = 0, diff = 0, condition = "xVNS"))
+df_samples_isi20$condition <-
+  factor(df_samples_isi20$condition, levels = c("taVNS", "sVNS", "xVNS"))
 
-df_samples$condition <-
-  factor(df_samples$condition, levels = c("taVNS", "sVNS", "xVNS"))
+df_samples_isi22 <- df_samples_isi22 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "taVNS"))
+df_samples_isi22 <- df_samples_isi22 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "sVNS"))
+df_samples_isi22 <- df_samples_isi22 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "xVNS"))
+df_samples_isi22$condition <-
+  factor(df_samples_isi22$condition, levels = c("taVNS", "sVNS", "xVNS"))
 
-df_samples_isi22 <- df_samples
+df_samples_isi24 <- df_samples_isi24 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "taVNS"))
+df_samples_isi24 <- df_samples_isi24 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "sVNS"))
+df_samples_isi24 <- df_samples_isi24 %>%
+  add_row(data.frame(mu = 0, diff = 0, condition = "xVNS"))
+df_samples_isi24$condition <-
+  factor(df_samples_isi24$condition, levels = c("taVNS", "sVNS", "xVNS"))
 
 # sham vs real -----------------------------------------------------------------
-r <- fit_and_compare2(df_real_stim$ISI20_diff, "(Real stim - real pre)",
-                      df_sham_stim$ISI20_diff, "(Sham stim - sham pre)")
-r <- fit_and_compare2(df_real_stim$ISI22_diff, "(Real stim - real pre)",
-                      df_sham_stim$ISI22_diff, "(Sham stim - sham pre)")
-r <- fit_and_compare2(df_real_stim$ISI24_diff, "(Real stim - real pre)",
-                      df_sham_stim$ISI24_diff, "(Sham stim - sham pre)")
+r <- fit_and_compare2(
+  df_real_stim$ISI20_diff, "(Real stim - real pre)",
+  df_sham_stim$ISI20_diff, "(Sham stim - sham pre)"
+)
+r <- fit_and_compare2(
+  df_real_stim$ISI22_diff, "(Real stim - real pre)",
+  df_sham_stim$ISI22_diff, "(Sham stim - sham pre)"
+)
+r <- fit_and_compare2(
+  df_real_stim$ISI24_diff, "(Real stim - real pre)",
+  df_sham_stim$ISI24_diff, "(Sham stim - sham pre)"
+)
 
-r <- fit_and_compare2(df_real_post$ISI20_diff, "(Real post - real pre)",
-                      df_sham_post$ISI20_diff, "(Sham post - sham pre)")
-r <- fit_and_compare2(df_real_post$ISI22_diff, "(Real post - real pre)",
-                      df_sham_post$ISI22_diff, "(Sham post - sham pre)")
-r <- fit_and_compare2(df_real_post$ISI24_diff, "(Real post - real pre)",
-                      df_sham_post$ISI24_diff, "(Sham post - sham pre)")
+r <- fit_and_compare2(
+  df_real_post$ISI20_diff, "(Real post - real pre)",
+  df_sham_post$ISI20_diff, "(Sham post - sham pre)"
+)
+r <- fit_and_compare2(
+  df_real_post$ISI22_diff, "(Real post - real pre)",
+  df_sham_post$ISI22_diff, "(Sham post - sham pre)"
+)
+r <- fit_and_compare2(
+  df_real_post$ISI24_diff, "(Real post - real pre)",
+  df_sham_post$ISI24_diff, "(Sham post - sham pre)"
+)
 
 # no vs real -------------------------------------------------------------------
-r <- fit_and_compare2(df_real_stim$ISI20_diff, "(Real stim - real pre)",
-                      df_no_stim$ISI20_diff, "(No stim - no pre)")
-r <- fit_and_compare2(df_real_stim$ISI22_diff, "(Real stim - real pre)",
-                      df_no_stim$ISI22_diff, "(No stim - no pre)")
-r <- fit_and_compare2(df_real_stim$ISI24_diff, "(Real stim - real pre)",
-                      df_no_stim$ISI24_diff, "(No stim - no pre)")
+r <- fit_and_compare2(
+  df_real_stim$ISI20_diff, "(Real stim - real pre)",
+  df_no_stim$ISI20_diff, "(No stim - no pre)"
+)
+r <- fit_and_compare2(
+  df_real_stim$ISI22_diff, "(Real stim - real pre)",
+  df_no_stim$ISI22_diff, "(No stim - no pre)"
+)
+r <- fit_and_compare2(
+  df_real_stim$ISI24_diff, "(Real stim - real pre)",
+  df_no_stim$ISI24_diff, "(No stim - no pre)"
+)
 
-r <- fit_and_compare2(df_real_post$ISI20_diff, "(Real post - real pre)",
-                      df_no_post$ISI20_diff, "(No post - no pre)")
-r <- fit_and_compare2(df_real_post$ISI22_diff, "(Real post - real pre)",
-                      df_no_post$ISI22_diff, "(No post - no pre)")
-r <- fit_and_compare2(df_real_post$ISI24_diff, "(Real post - real pre)",
-                      df_no_post$ISI24_diff, "(No post - no pre)")
+r <- fit_and_compare2(
+  df_real_post$ISI20_diff, "(Real post - real pre)",
+  df_no_post$ISI20_diff, "(No post - no pre)"
+)
+r <- fit_and_compare2(
+  df_real_post$ISI22_diff, "(Real post - real pre)",
+  df_no_post$ISI22_diff, "(No post - no pre)"
+)
+r <- fit_and_compare2(
+  df_real_post$ISI24_diff, "(Real post - real pre)",
+  df_no_post$ISI24_diff, "(No post - no pre)"
+)
 
 # no vs sham -------------------------------------------------------------------
-r <- fit_and_compare2(df_sham_stim$ISI20_diff, "(Sham stim - sham pre)",
-                      df_no_stim$ISI20_diff, "(No stim - no pre)")
-r <- fit_and_compare2(df_sham_stim$ISI22_diff, "(Sham stim - sham pre)",
-                      df_no_stim$ISI22_diff, "(No stim - no pre)")
-r <- fit_and_compare2(df_sham_stim$ISI24_diff, "(Sham stim - sham pre)",
-                      df_no_stim$ISI24_diff, "(No stim - no pre)")
+r <- fit_and_compare2(
+  df_sham_stim$ISI20_diff, "(Sham stim - sham pre)",
+  df_no_stim$ISI20_diff, "(No stim - no pre)"
+)
+r <- fit_and_compare2(
+  df_sham_stim$ISI22_diff, "(Sham stim - sham pre)",
+  df_no_stim$ISI22_diff, "(No stim - no pre)"
+)
+r <- fit_and_compare2(
+  df_sham_stim$ISI24_diff, "(Sham stim - sham pre)",
+  df_no_stim$ISI24_diff, "(No stim - no pre)"
+)
 
-r <- fit_and_compare2(df_sham_post$ISI20_diff, "(Sham post - sham pre)",
-                      df_no_post$ISI20_diff, "(No post - no pre)")
-r <- fit_and_compare2(df_sham_post$ISI22_diff, "(Sham post - sham pre)",
-                      df_no_post$ISI22_diff, "(No post - no pre)")
-r <- fit_and_compare2(df_sham_post$ISI24_diff, "(Sham post - sham pre)",
-                      df_no_post$ISI24_diff, "(No post - no pre)")
+r <- fit_and_compare2(
+  df_sham_post$ISI20_diff, "(Sham post - sham pre)",
+  df_no_post$ISI20_diff, "(No post - no pre)"
+)
+r <- fit_and_compare2(
+  df_sham_post$ISI22_diff, "(Sham post - sham pre)",
+  df_no_post$ISI22_diff, "(No post - no pre)"
+)
+r <- fit_and_compare2(
+  df_sham_post$ISI24_diff, "(Sham post - sham pre)",
+  df_no_post$ISI24_diff, "(No post - no pre)"
+)
 
 # pre comparison ---------------------------------------------------------------
 r <- fit_and_compare2(df_real_pre$ISI20, "Real", df_sham_pre$ISI20, "Sham")
--- normal_plot.R
+++ normal_plot.R
@@ -4,7 +4,9 @@ library(glue)
 # add type
 df_samples_rmt$type <- "RMT"
 df_samples_amt$type <- "AMT"
+df_samples_isi20$type <- "ISI20"
 df_samples_isi22$type <- "ISI22"
+df_samples_isi24$type <- "ISI24"
 df_samples_sici$type <- "AUC"
 
 # rmt and amt ------------------------------------------------------------------
@@ -17,39 +19,90 @@ ggplot(df_amt_rmt, aes(x = diff, y = mu, color = condition)) +
   stat_pointinterval() +
   ylab("Difference") +
   facet_grid(glue('Delta"{type}"') ~ condition,
-                  scales = "free_y", labeller = label_parsed) +
-  scale_x_continuous(name = "",
-                     breaks = c(0, 1, 2),
-                     labels = c("Pre", "During", "Post")) +
+    scales = "free_y", labeller = label_parsed
+  ) +
+  scale_x_continuous(
+    name = "",
+    breaks = c(0, 1, 2),
+    labels = c("Pre", "During", "Post")
+  ) +
   scale_color_manual(values = c("black", "black", "grey50")) +
   theme(legend.position = "none")
 
 ggsave("./fig/AMT_RMT_FINAL.tiff",
-       width = 1920,
-       height = 960,
-       dpi = 200,
-       units = "px")
+  width = 1920,
+  height = 960,
+  dpi = 200,
+  units = "px"
+)
 
 
 # sai --------------------------------------------------------------------------
+ggplot(df_samples_isi20, aes(x = diff, y = mu, color = condition)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
+  stat_pointinterval() +
+  ylab("Difference") +
+  facet_grid(glue('Delta*SAI["{type}"]') ~ condition,
+    scales = "free_y", labeller = label_parsed
+  ) +
+  scale_x_continuous(
+    name = "",
+    breaks = c(0, 1, 2),
+    labels = c("Pre", "During", "Post")
+  ) +
+  scale_color_manual(values = c("black", "black", "grey50")) +
+  theme(legend.position = "none")
+
+ggsave("./fig/SAI20_FINAL.tiff",
+  width = 1920,
+  height = 480,
+  dpi = 200,
+  units = "px"
+)
+
 ggplot(df_samples_isi22, aes(x = diff, y = mu, color = condition)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
   stat_pointinterval() +
   ylab("Difference") +
   facet_grid(glue('Delta*SAI["{type}"]') ~ condition,
-                  scales = "free_y", labeller = label_parsed) +
-  scale_x_continuous(name = "",
-                     breaks = c(0, 1, 2),
-                     labels = c("Pre", "During", "Post")) +
+    scales = "free_y", labeller = label_parsed
+  ) +
+  scale_x_continuous(
+    name = "",
+    breaks = c(0, 1, 2),
+    labels = c("Pre", "During", "Post")
+  ) +
   scale_color_manual(values = c("black", "black", "grey50")) +
   theme(legend.position = "none")
 
-ggsave("./fig/SAI_FINAL.tiff",
-       width = 1920,
-       height = 480,
-       dpi = 200,
-       units = "px")
+ggsave("./fig/SAI22_FINAL.tiff",
+  width = 1920,
+  height = 480,
+  dpi = 200,
+  units = "px"
+)
 
+ggplot(df_samples_isi24, aes(x = diff, y = mu, color = condition)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
+  stat_pointinterval() +
+  ylab("Difference") +
+  facet_grid(glue('Delta*SAI["{type}"]') ~ condition,
+    scales = "free_y", labeller = label_parsed
+  ) +
+  scale_x_continuous(
+    name = "",
+    breaks = c(0, 1, 2),
+    labels = c("Pre", "During", "Post")
+  ) +
+  scale_color_manual(values = c("black", "black", "grey50")) +
+  theme(legend.position = "none")
+
+ggsave("./fig/SAI24_FINAL.tiff",
+  width = 1920,
+  height = 480,
+  dpi = 200,
+  units = "px"
+)
 
 # sici -------------------------------------------------------------------------
 ggplot(df_samples_sici, aes(x = diff, y = mu, color = condition)) +
@@ -57,15 +110,19 @@ ggplot(df_samples_sici, aes(x = diff, y = mu, color = condition)) +
   stat_pointinterval() +
   ylab("Difference") +
   facet_grid(glue('Delta*SICI["{type}"]') ~ condition,
-                  scales = "free_y", labeller = label_parsed) +
-  scale_x_continuous(name = "",
-                     breaks = c(0, 1, 2),
-                     labels = c("Pre", "During", "Post")) +
+    scales = "free_y", labeller = label_parsed
+  ) +
+  scale_x_continuous(
+    name = "",
+    breaks = c(0, 1, 2),
+    labels = c("Pre", "During", "Post")
+  ) +
   scale_color_manual(values = c("black", "black", "grey50")) +
   theme(legend.position = "none")
 
 ggsave("./fig/SICI_FINAL.tiff",
-       width = 1920,
-       height = 480,
-       dpi = 200,
-       units = "px")
+  width = 1920,
+  height = 480,
+  dpi = 200,
+  units = "px"
+)
